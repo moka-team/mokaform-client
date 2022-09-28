d02724e882f5b975a2103da6da55d6fafee51e9e
Cho Si Yeon <tldusdk5@naver.com>
2022-09-28 13:52:49 +0900

    refactor: SignUpForm.jsx 코드 수정 (MOKA-34)

--- src/components/users/SignUp/SignUpForm.jsx
+++ src/components/users/SignUp/SignUpForm.jsx
@@ -3,8 +3,48 @@ import AgeRow from "./AgeRow";
 import InterestRow from "./InterestRow";
 import JobRow from "./JobRow";
 import SexRow from "./SexRow";
-import "./SignUpForm.css";
 import SignEssentialForm from "./SignEssentialForm";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center;
+
+  &.center {
+    background-color: #f9fafb;
+  }
+`;
+
+const Row = styled.div`
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+
+const Button = styled.button`
+  width: 415px;
+  height: 50px;
+  margin: 10px;
+  margin-top: 45px;
+  border-radius: 10px;
+  border: none;
+  background-color: #286bd0;
+  color: white;
+  font-weight: bold;
+  &:hover {
+    background-color: #0064ff;
+  }
+`;
+
+const Title = styled.h1`
+  margin: 10px;
+  margin-top: 35px;
+  margin-bottom: 25px;
+  font-weight: 700;
+`;
 
 export default function SignUpForm() {
   const signOptionRef = useRef(null);
@@ -19,46 +59,28 @@ export default function SignUpForm() {
   const onCompleteBtnClickHandler = (event) => {};
 
   return (
-    <div>
-      <>
-        <div className="sign_essential_form_container">
-          <div className="row_div">
-            <h1 className="row_title2">SIGN UP | MOKA FORM</h1>
-            <SignEssentialForm></SignEssentialForm>
-            <div>
-              <button type="button" onClick={onClickHandler} className="button">
-                다음
-              </button>
-            </div>
-          </div>
-        </div>
-        <div ref={signOptionRef} className="sign_option_container">
-          <div className="row_div">
-            <AgeRow></AgeRow>
-            <SexRow></SexRow>
-            <button
-              type="button"
-              onClick={onNextBtnClickHandler}
-              className="button"
-            >
-              다음
-            </button>
-          </div>
-        </div>
-        <div ref={signOptionRef2} className="sign_option_container2">
-          <div className="row_div">
-            <JobRow></JobRow>
-            <InterestRow></InterestRow>
-            <button
-              type="button"
-              onClick={onCompleteBtnClickHandler}
-              className="button"
-            >
-              가입 완료
-            </button>
-          </div>
-        </div>
-      </>
-    </div>
+    <>
+      <Container>
+        <Row>
+          <Title>SIGN UP | MOKA FORM</Title>
+          <SignEssentialForm></SignEssentialForm>
+          <Button onClick={onClickHandler}>다음</Button>
+        </Row>
+      </Container>
+      <Container ref={signOptionRef} className="center">
+        <Row>
+          <AgeRow></AgeRow>
+          <SexRow></SexRow>
+          <Button onClick={onNextBtnClickHandler}>다음</Button>
+        </Row>
+      </Container>
+      <Container ref={signOptionRef2}>
+        <Row>
+          <JobRow></JobRow>
+          <InterestRow></InterestRow>
+          <Button onClick={onCompleteBtnClickHandler}>가입 완료</Button>
+        </Row>
+      </Container>
+    </>
   );
 }
