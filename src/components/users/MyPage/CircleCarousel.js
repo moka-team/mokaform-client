878673f2c75f8b4ee9b1fb32b466055436358818
SeonDahye <81613151+SeonDahye@users.noreply.github.com>
2022-10-15 14:04:35 +0900

    feat: 관심사 hover 처리

--- src/components/users/MyPage/CircleCarousel.js
+++ src/components/users/MyPage/CircleCarousel.js
@@ -11,9 +11,19 @@ import {
   faPersonCircleQuestion,
 } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import { display } from "@mui/system";
 
 // 임시 유저 관심사 데이터
-const preferenceList = ["HOBBY", "IT", "PET"];
+const preferenceList = [
+  "HOBBY",
+  "IT",
+  "PET",
+  "PSYCHOLOGY",
+  "SOCIAL_POLITICS",
+  "LEARNING",
+  "PREFERENCE_RESEARCH",
+  "LIFE",
+];
 
 const Container = styled.div`
   display: grid;
@@ -42,33 +52,54 @@ const Card = styled.div`
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1), 0 4px 4px rgba(0, 0, 0, 0.19);
   &:hover {
     background-color: gray;
+    text-align: center;
+    color: white;
+    font-weight: 600;
   }
 `;
 
 function CircleCarousel() {
+  const [ishover1, setIshover1] = useState(false);
+  const [ishover2, setIshover2] = useState(false);
+  const [ishover3, setIshover3] = useState(false);
+  const [ishover4, setIshover4] = useState(false);
+  const [ishover5, setIshover5] = useState(false);
+  const [ishover6, setIshover6] = useState(false);
+  const [ishover7, setIshover7] = useState(false);
+  const [ishover8, setIshover8] = useState(false);
+
   return (
     <Container>
       <Card
+        onMouseOver={() => setIshover1(true)}
+        onMouseOut={() => setIshover1(false)}
         style={{ display: preferenceList.includes("PET") ? "flex" : "none" }}
       >
         <FontAwesomeIcon
           icon={faPaw}
           size={"4x"}
           style={{
+            display: ishover1 ? "none" : "flex",
             color: "#636870",
           }}
         />
+        <p style={{ display: ishover1 ? "flex" : "none" }}>반려동물</p>
       </Card>
       <Card
+        onMouseOver={() => setIshover2(true)}
+        onMouseOut={() => setIshover2(false)}
         style={{ display: preferenceList.includes("LIFE") ? "flex" : "none" }}
       >
         <FontAwesomeIcon
           icon={faFaceSmile}
           size={"4x"}
-          style={{ color: "#636870" }}
+          style={{ display: ishover2 ? "none" : "flex", color: "#636870" }}
         />
+        <p style={{ display: ishover2 ? "flex" : "none" }}>일상</p>
       </Card>
       <Card
+        onMouseOver={() => setIshover3(true)}
+        onMouseOut={() => setIshover3(false)}
         style={{
           display: preferenceList.includes("SOCIAL_POLITICS") ? "flex" : "none",
         }}
@@ -76,10 +107,13 @@ function CircleCarousel() {
         <FontAwesomeIcon
           icon={faHashtag}
           size={"4x"}
-          style={{ color: "#636870" }}
+          style={{ display: ishover3 ? "none" : "flex", color: "#636870" }}
         />
+        <p style={{ display: ishover3 ? "flex" : "none" }}>사회·정치</p>
       </Card>
       <Card
+        onMouseOver={() => setIshover4(true)}
+        onMouseOut={() => setIshover4(false)}
         style={{
           display: preferenceList.includes("LEARNING") ? "flex" : "none",
         }}
@@ -87,28 +121,37 @@ function CircleCarousel() {
         <FontAwesomeIcon
           icon={faBook}
           size={"4x"}
-          style={{ color: "#636870" }}
+          style={{ display: ishover4 ? "none" : "flex", color: "#636870" }}
         />
+        <p style={{ display: ishover4 ? "flex" : "none" }}>학습</p>
       </Card>
       <Card
+        onMouseOver={() => setIshover5(true)}
+        onMouseOut={() => setIshover5(false)}
         style={{ display: preferenceList.includes("IT") ? "flex" : "none" }}
       >
         <FontAwesomeIcon
           icon={faLaptopCode}
           size={"4x"}
-          style={{ color: "#636870" }}
+          style={{ display: ishover5 ? "none" : "flex", color: "#636870" }}
         />
+        <p style={{ display: ishover5 ? "flex" : "none" }}>IT</p>
       </Card>
       <Card
+        onMouseOver={() => setIshover6(true)}
+        onMouseOut={() => setIshover6(false)}
         style={{ display: preferenceList.includes("HOBBY") ? "flex" : "none" }}
       >
         <FontAwesomeIcon
           icon={faGuitar}
           size={"4x"}
-          style={{ color: "#636870" }}
+          style={{ display: ishover6 ? "none" : "flex", color: "#636870" }}
         />
+        <p style={{ display: ishover6 ? "flex" : "none" }}>취미</p>
       </Card>
       <Card
+        onMouseOver={() => setIshover7(true)}
+        onMouseOut={() => setIshover7(false)}
         style={{
           display: preferenceList.includes("PREFERENCE_RESEARCH")
             ? "flex"
@@ -118,10 +161,13 @@ function CircleCarousel() {
         <FontAwesomeIcon
           icon={faMagnifyingGlass}
           size={"4x"}
-          style={{ color: "#636870" }}
+          style={{ display: ishover7 ? "none" : "flex", color: "#636870" }}
         />
+        <p style={{ display: ishover7 ? "flex" : "none" }}>선호도 조사</p>
       </Card>
       <Card
+        onMouseOver={() => setIshover8(true)}
+        onMouseOut={() => setIshover8(false)}
         style={{
           display: preferenceList.includes("PSYCHOLOGY") ? "flex" : "none",
         }}
@@ -129,8 +175,9 @@ function CircleCarousel() {
         <FontAwesomeIcon
           icon={faPersonCircleQuestion}
           size={"4x"}
-          style={{ color: "#636870" }}
+          style={{ display: ishover8 ? "none" : "flex", color: "#636870" }}
         />
+        <p style={{ display: ishover8 ? "flex" : "none" }}>심리</p>
       </Card>
     </Container>
   );
