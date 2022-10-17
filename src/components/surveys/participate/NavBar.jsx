269019920910ae8fc6cd684ffb52abf3be35f770
Cho Si Yeon <tldusdk5@naver.com>
2022-10-17 16:22:10 +0900

    feat: sharingUrl 추가

--- src/components/surveys/participate/NavBar.jsx
+++ src/components/surveys/participate/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SNavBar, SaveBtn } from "./styled";
 import { useRecoilState, useRecoilValue } from "recoil";
 import {
