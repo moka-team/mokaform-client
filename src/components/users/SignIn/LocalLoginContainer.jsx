25999ae1757a19ad06f04b79ad3bcb3ca06a9e48
SeonDahye <81613151+SeonDahye@users.noreply.github.com>
2022-10-17 14:35:31 +0900

    feat: 로그인 구현
    - 로컬 스토리지에 recoil-persist 저장
    - axios로 연결

--- src/components/users/SignIn/LocalLoginContainer.jsx
+++ src/components/users/SignIn/LocalLoginContainer.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import { styled } from "@mui/material/styles";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import {
   LocalLoginWrapper,
   LoginInputContainer,
   LoginButton,
 } from "./SigninStyle";
+import { useRecoilState } from "recoil";
+import { userState } from "../../../authentication/userState";
 
 const CustomTextField = styled((props) => (
   <TextField InputProps={{ disableUnderline: true }} {...props} />
@@ -38,6 +42,9 @@ function LocalLoginContainer() {
     password: "",
   });
 
+  const [user, setUser] = useRecoilState(userState);
+  const navigate = useNavigate();
+
   const handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
@@ -46,14 +53,36 @@ function LocalLoginContainer() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert("email: " + inputs.email + "\n" + "password: " + inputs.password);
+
+    axios
+      .post("/api/v1/users/login", {
+        email: inputs.email,
+        password: inputs.password,
+      })
+      .then(function (response) {
+        if (response.data.message.includes("성공")) {
+          // 로그인 성공 시 로컬 스토리지에 저장
+          localStorage.clear();
+          // 로그인 성공 시 유저 데이터 저장
+          setUser(response.data.data);
+
+          window.alert("로그인이 완료되었습니다.");
+          navigate("/");
+        } else if (response.data.code === "U001") {
+          alert("이메일 또는 비밀번호가 일치하지 않습니다.");
+        } else {
+          window.alert("로그인 에러 발생");
+        }
+      })
+      .catch(function (error) {
+        window.alert("이메일 또는 비밀번호가 일치하지 않습니다.");
+      });
   };
 
   return (
     <LocalLoginWrapper>
       <form onSubmit={handleSubmit}>
         <LoginInputContainer>
-         
           <CustomTextField
             type="text"
             name="email"
--- src/pages/users/myPage/index.js
+++ src/pages/users/myPage/index.js
@@ -3,7 +3,9 @@ import Header from "../../../components/users/MyPage/MypageHeader";
 import Profile from "../../../components/users/MyPage/Profile";
 import Separator from "../../../components/users/MyPage/Separator";
 import ListContainer from "../../../components/users/MyPage/ListContainer";
-
+import { useEffect } from "react";
+import { useRecoilValue } from "recoil";
+import { userState } from "../../../authentication/userState";
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -17,6 +19,14 @@ const Wrapper = styled.div`
 `;
 
 function Mypage() {
+  // 로그인 상태 검사
+  const user = useRecoilValue(userState);
+  useEffect(() => {
+    if (user === null) {
+      alert("로그인이 필요한 서비스입니다.");
+      window.location.replace("http://localhost:3000/");
+    }
+  }, []);
   return (
     <Container>
       <Header />
