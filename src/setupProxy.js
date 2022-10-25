6ed25d9b2b06a52a60db20b7406c0906fd50f4bc
Seyiul <63714074+Seyiul@users.noreply.github.com>
2022-10-25 18:22:58 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://210.109.60.220:8080",
+      target: "http://210.109.60.220:8080/api",
       changeOrigin: true,
     })
   );
