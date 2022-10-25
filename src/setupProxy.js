201be649803883bb871759c0f872f3c6739d1fe3
Seyiul <63714074+Seyiul@users.noreply.github.com>
2022-10-25 20:44:54 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,13 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/api", {
-      target: "http://http://ip-210-109-60-220.c.kr-central-1.kakaokic.com.",
-      changeOrigin: true,
-    })
+    cors(
+      createProxyMiddleware("/api", {
+        target: "http://http://ip-210-109-60-220.c.kr-central-1.kakaokic.com.",
+        changeOrigin: true,
+      })
+    )
   );
 };
