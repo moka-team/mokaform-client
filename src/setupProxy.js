b702863650a1e5f9fadbaba6364a7f00c1d3ec8f
seyiul <iseuli713@gmail.com>
2022-10-26 08:57:15 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://ip-210-109-60-220.c.kr-central-1.kakaokic.com.",
+      target: "http://ip-210-109-60-220.c.kr-central-1.kakaokic.com.:8080",
       changeOrigin: true,
     })
   );
