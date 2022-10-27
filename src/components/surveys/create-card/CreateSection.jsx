799d9da41bb05435b133829480131fe9381abef4
Cho Si Yeon <tldusdk5@naver.com>
2022-10-28 08:57:02 +0900

    style: createSection 너비 조정

--- src/components/surveys/create-card/CreateSection.jsx
+++ src/components/surveys/create-card/CreateSection.jsx
@@ -1,13 +1,30 @@
 import React from "react";
-import { Create, Summary, Title } from "../create/styled";
+import { Summary, Title } from "../create/styled";
 import { surveyTitle, surveySummary, surveyListState } from "../../../atoms";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import SurveyCreateItem from "./SurveyCreateItem";
 import SurveyItemCreator from "./SurveyItemCreator";
+import styled from "styled-components";
 export default function CreateSection() {
   const surveyList = useRecoilValue(surveyListState);
   const setTitle = useSetRecoilState(surveyTitle);
   const setSummary = useSetRecoilState(surveySummary);
+
+  const Create = styled.div`
+    display: flex;
+    width: 35%;
+    min-height: 100%;
+    flex-direction: column;
+    background-color: #202632;
+    color: white;
+    float: left;
+    padding: 2% 2%;
+    h1 {
+      font-size: large;
+      font-weight: 800;
+      margin-top: 25px;
+    }
+  `;
 
   const titleOnChange = (event) => {
     setTitle(event.target.value);
