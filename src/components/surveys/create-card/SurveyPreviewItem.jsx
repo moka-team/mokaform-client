aa63ad3a30437f1da0155fa78a5f8aecb5aa9f36
SeonDahye <81613151+SeonDahye@users.noreply.github.com>
2022-10-28 19:02:31 +0900

    feat: 프리뷰 CSS 적용

--- src/components/surveys/create-card/SurveyPreviewItem.jsx
+++ src/components/surveys/create-card/SurveyPreviewItem.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import { surveyListState, detailMCQuestionState } from "../../../atoms";
 import { useRecoilValue } from "recoil";
-import { QuestionWrapper, QuestionOption, QuestionText } from "./styled";
+import {
+  QuestionWrapper,
+  QuestionOption,
+  QuestionText,
+  OptionWrapper,
+} from "./styled";
 
 export default function SurveyPreviewItem({ item }) {
   const detailQuestionList = useRecoilValue(detailMCQuestionState);
   const surveyList = useRecoilValue(surveyListState);
   const index = surveyList.findIndex((listItem) => listItem === item);
   return (
-    <QuestionWrapper>
-      <QuestionText color="#0064ff">Q{index + 1}</QuestionText>
-      <QuestionText color="black">{item.title}</QuestionText>
-      {detailQuestionList.map((detailQuestionItem) =>
-        item.index === detailQuestionItem.questionIndex ? (
-          <QuestionOption>{detailQuestionItem.content}</QuestionOption>
-        ) : (
-          <></>
-        )
-      )}
-    </QuestionWrapper>
+    <>
+      <QuestionWrapper>
+        <QuestionText color="#0064ff">Q{index + 1}</QuestionText>
+        <QuestionText color="white">{item.title}</QuestionText>
+        {detailQuestionList.map((detailQuestionItem) =>
+          item.index === detailQuestionItem.questionIndex ? (
+            <QuestionOption>{detailQuestionItem.content}</QuestionOption>
+          ) : (
+            <></>
+          )
+        )}
+      </QuestionWrapper>
+    </>
   );
 }
