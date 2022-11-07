48c2862eec3e0e4a2f95557241d0961df7a59044
seyiul <iseuli713@gmail.com>
2022-11-07 10:51:53 +0900

    Update LocalLoginContainer.jsx

--- src/components/users/signIn/LocalLoginContainer.jsx
+++ src/components/users/signIn/LocalLoginContainer.jsx
@@ -47,10 +47,7 @@ function LocalLoginContainer() {
         password: inputs.password,
       })
       .then(function (response) {
-        const token = response.headers.Authorization;
-        console.log(response)
-        console.log(response.headers)
-        console.log(response.headers.get("Authorization"))
+        const token = response.headers.get("Authorization");
         console.log(token)
         if (response.data.message.includes("성공")) {
           // 로그인 성공 시 로컬 스토리지에 저장
