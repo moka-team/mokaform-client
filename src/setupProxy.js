852881b4f99257166cb6d99c2df3ac36df4c25be
Seyiul <63714074+Seyiul@users.noreply.github.com>
2022-12-03 23:28:40 +0900

    fix : cors 에러

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,16 +1,18 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
   app.use(
-    createProxyMiddleware('/analysis/', {
-      target: 'https://42d1adfb-5b9c-4cb1-8351-670c4a4c3b25.api.kr-central-1.kakaoi.io/ai/conversation/f54447ad459c4079880862d1b333d9e6',
+    createProxyMiddleware("/analysis", {
+      target:
+        "https://42d1adfb-5b9c-4cb1-8351-670c4a4c3b25.api.kr-central-1.kakaoi.io/ai/conversation/f54447ad459c4079880862d1b333d9e6",
       changeOrigin: true,
-    }),
+      pathRewrite: { "^/analysis": "" },
+    })
   );
   app.use(
-    createProxyMiddleware('/api/', {
-      target: 'http://api.mokaform.site:8080/',
+    createProxyMiddleware("/api/", {
+      target: "http://api.mokaform.site:8080/",
       changeOrigin: true,
-    }),
+    })
   );
 };
